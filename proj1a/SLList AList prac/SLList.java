60ccb430e7bfc55d6dd92577bc6fcc0b0be1adba
Yu Chun Fang <yuchun.fang@junyiacademy.org>
2021-05-07 01:38:33 +0800

    Practice SLList with sentinel node

--- proj1a/SLList AList prac/SLList.java
+++ proj1a/SLList AList prac/SLList.java
@@ -8,43 +8,57 @@ public class SLList {
         }
     }
 
-    private IntNode first; 
+    private IntNode sentinel; 
     private int size;
 
+    // 創建empty SLList
+    public SLList(){
+        sentinel = new IntNode(0,null);
+        size = 0;
+    }
+
     public SLList(int x) {
-        first = new IntNode(x, null);
+        sentinel = new IntNode(0, null);
+        sentinel.next = new IntNode(x, null);
+        size = 1;
     }
 
 
     /** Adds an item to the front of the list. */
     public void addFirst(int x) {
-        first = new IntNode(x, first);
+        sentinel.next = new IntNode(x, sentinel.next);
+        size += 1;
     }    
 
     /** Retrieves the front item from the list. */
     public int getFirst() {
-        return first.item;
+        return sentinel.next.item;
     }
 
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
         /* Your Code Here! */
-        IntNode pointer = first;
+        IntNode pointer = sentinel.next;
 
         while(pointer.next != null){
             pointer = pointer.next;
         }
         pointer.next = new IntNode(x, null);
+        size += 1;
     }
+    public int getLast() {
+        /* Your Code Here! */
+        IntNode pointer = sentinel.next;
+
+        while(pointer.next != null){
+            pointer = pointer.next;
+        }
+        return pointer.item;
+    }
+
 
     /** Returns the number of items in the list using recursion. */
     public int size() {
-        /* Your Code Here! */
-        IntNode pointer = first;
-        while(pointer != null){
-            size+=1;
-            pointer = pointer.next;
-        }
         return size;
     }
 
@@ -53,7 +67,20 @@ public class SLList {
         SLList x = new SLList(15);
         x.addFirst(5);
         x.addFirst(10);
+        
         System.out.println(x);
+        System.out.println(x.getFirst());
+        System.out.println(x.size());
+
+        System.out.println("----------------------------");
+
+        x.addFirst(1);
+        System.out.println(x.getFirst());
+        System.out.println(x.size());
+
+        System.out.println("----------------------------");
+        x.addLast(6);
+        System.out.println(x.getLast());
         System.out.println(x.size());
     }
 }
