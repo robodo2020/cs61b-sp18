17173acff40ef2afa8784ccb2117242112d80601
Yu Chun Fang <yuchun.fang@junyiacademy.org>
2021-05-30 00:01:44 +0800

    Fix: Fix null pointrer Exception

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,10 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            A = B;
+            return A;
+        }
         IntList cur = A;
         while(cur.rest != null) {
             cur = cur.rest;
