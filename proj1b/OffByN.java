8b72d37ddb27a7b2b659981f5898eb7764b48f30
Yu Chun Fang <declan.fang@g-mail.nsysu.edu.tw>
2021-06-14 18:46:20 +0800

    Feat: Add OffByN class and equalChars func for checking char diff by input

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,2 +1,11 @@
-public class OffByN {
+public class OffByN implements CharacterComparator{
+    private int dif_num;
+    public OffByN(int N){
+        dif_num = N;
+    }
+    @Override
+    public boolean equalChars(char x, char y) {
+        int diff = x - y;
+        return (diff == dif_num || diff == -dif_num);
+    }
 }
