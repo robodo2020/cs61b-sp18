6a79755a7ce1031e357f1c52ac1c238de6999a14
Yu Chun Fang <declan.fang@g-mail.nsysu.edu.tw>
2021-06-14 18:47:39 +0800

    Feat: Complete isPalindrome func for OffByOne/OffByN

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -24,6 +24,7 @@ public class Palindrome {
 //        }
 //        return true;
     }
+
     private boolean recurPalindrome(Deque checkWord){
         // base case
         if (checkWord.size() < 2) {
@@ -36,5 +37,23 @@ public class Palindrome {
         return (front == last) && recurPalindrome(checkWord);
     }
 
+    public boolean isPalindrome(String word, CharacterComparator cc){
+        Deque checkWord = wordToDeque(word);
+        return recurPalindrome(checkWord, cc);
 
+        // flake should return true
+    }
+
+    private boolean recurPalindrome(Deque checkWord, CharacterComparator cc){
+        if (checkWord.size() < 2){
+            return true;
+        }
+        char front = checkWord.removeFirst().toString().charAt(0);
+        char last = checkWord.removeLast().toString().charAt(0);
+        // the code above transform the class from Object -> String -> char
+        // solution from here: https://stackoverflow.com/questions/13628312/conversion-from-object-to-char-in-java
+        // should figure out Object and the solution without transforming
+
+        return cc.equalChars(front, last) && recurPalindrome(checkWord, cc);
+    }
 }
