fa58c73d0e1a4b70c1d34f3f9c5e4782248c6d76
Yu Chun Fang <declan.fang@g-mail.nsysu.edu.tw>
2021-06-14 18:57:35 +0800

    Test: Add tests for isPalindrome when using OffByN

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -27,4 +27,28 @@ public class TestPalindrome {
         assertFalse((palindrome.isPalindrome(" xcGwiIiwGcx")));
         assertTrue((palindrome.isPalindrome(" xcGwiIiwGcx ")));
     }
+
+    @Test
+    public void testisPalindromeOffByOne(){
+        CharacterComparator cc = new OffByOne();
+        // Get understand about cc whether the object is OffByOne or CharacterComparator?
+        assertTrue(palindrome.isPalindrome("flake", cc));
+        assertTrue(palindrome.isPalindrome("acb", cc));
+        assertTrue(palindrome.isPalindrome("x", cc));
+        assertTrue(palindrome.isPalindrome("%&", cc));
+        assertFalse(palindrome.isPalindrome("aaaa", cc));
+        assertFalse(palindrome.isPalindrome("abcd", cc));
+    }
+    @Test
+    public void testisPalindromeOffByN(){
+        CharacterComparator cc = new OffByN(5);
+        assertTrue(palindrome.isPalindrome("toity", cc));
+        assertTrue(palindrome.isPalindrome("augylpf", cc));
+        assertTrue(palindrome.isPalindrome("liang", cc));
+        assertFalse(palindrome.isPalindrome("aaaa", cc));
+        assertFalse(palindrome.isPalindrome("flake", cc));
+        assertFalse(palindrome.isPalindrome("acb", cc));
+
+
+    }
 }
